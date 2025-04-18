d40b3b6471ce1aaa336862c0e7c9badc0b0a876a
Your Name <user@example.com>
2025-04-18 22:31:08 +0300

    Fix import error by exporting ViewSets in views/__init__.py

--- api/views/__init__.py
+++ api/views/__init__.py
@@ -1,3 +1,33 @@
 """
 Paquete de vistas para la API.
 """
+
+# Importar todos los ViewSets desde el archivo views.py principal
+from ..views import (
+    ClassViewSet,
+    SectionViewSet,
+    SubjectViewSet,
+    StudentViewSet,
+    ScheduleViewSet,
+    AttendanceViewSet,
+    AssignmentViewSet,
+    AssignmentSubmissionViewSet,
+    GradeViewSet,
+    NoteViewSet,
+    NotificationViewSet
+)
+
+# Exportar todos los ViewSets para que sean accesibles desde api.views
+__all__ = [
+    'ClassViewSet',
+    'SectionViewSet',
+    'SubjectViewSet',
+    'StudentViewSet',
+    'ScheduleViewSet',
+    'AttendanceViewSet',
+    'AssignmentViewSet',
+    'AssignmentSubmissionViewSet',
+    'GradeViewSet',
+    'NoteViewSet',
+    'NotificationViewSet'
+]
