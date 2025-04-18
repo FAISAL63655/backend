35f9151aa5fc3843aec107a0d9a396fa0b8301f4
Your Name <user@example.com>
2025-04-18 22:28:53 +0300

    Fix import error by moving media_views.py to main directory

--- config/urls.py
+++ config/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
-from api.views.media_views import serve_media_file
+from api.media_views import serve_media_file
 
 urlpatterns = [
     path('admin/', admin.site.urls),
